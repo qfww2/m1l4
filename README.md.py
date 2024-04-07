25b919e091dc6fb96c2b8c4586ca689e7dcf2193
qfww2 <161402544+qfww2@users.noreply.github.com>
2024-04-07 00:45:36 -0500

    Update README.md.py

--- README.md.py
+++ README.md.py
@@ -1,4 +1,5 @@
 import discord
+import random 
 from discord.ext import commands
 
 intents = discord.Intents.default()
@@ -27,5 +28,46 @@ async def slap(ctx, members: commands.Greedy[discord.Member], *, reason='because
     slapped = " and ".join(x.name for x in members)
     await ctx.send(f'{slapped} just got slapped {reason}')
 
-bot.run("MTIxNjkyMjkyNDk5MzIxNjUyMg.GN4JL0.6a1Qs8ooAnQGNB26zHaFbPatwzkmRZaaDeVA_k")
+problem = [
+    "Calentamiento global.",
+    "Lluvia acida.",
+    "Deterioro del oxigeno",
+    "Menor prosperidad de vida",
+    "Calles sucias y en pesimo estado",
+    "Incendios forestales"
+]
+
+@bot.command()
+async def problemas(ctx):
+    consejo = random.choice(problem)
+    await ctx.send(consejo)
+
+@bot.command()
+async def reciclaje(ctx):
+    with open('image/r1.jpg', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+    await ctx.send(" escoge el producto con menor envase o envases que puedas reciclar. Separa la basura orgánica de los envases. Separa los envases dependiendo de su material: plásticos, vidrio, aluminio o metal. Coloca cada envase en el contenedor que corresponde.")
+
+@bot.command()
+async def contaminacion(ctx):
+    with open('image/r2.jpg', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+    await ctx.send("Se entiende por contaminación ambiental cuando existe la presencia de sustancias nocivas en el agua, aire o suelo.")
+
+@bot.command()
+async def mem1(ctx):
+    with open('meme/sus.png', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+@bot.command()
+async def mem2(ctx):
+    with open('meme/troll.png', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+
+bot.run("tu token xd")
 
